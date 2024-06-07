0824448d82c1eecbfe70bf966757ad9a7605dd92
Justin Pridgen <justincpridgen672@gmail.com>
2024-06-07 16:17:17 -0400

    v1.0.1

--- changelog.md
+++ changelog.md
@@ -1,3 +1,6 @@
 # NA Demon Changelog
+## v1.0.1 (2024-06-07)
+- Ported to Geometry Dash v2.206
+
 ## v1.0.0 (2024-05-20)
 - Initial release
--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ class $modify(NDLevelInfoLayer, LevelInfoLayer) {
     bool init(GJGameLevel* level, bool challenge) {
         if (!LevelInfoLayer::init(level, challenge)) return false;
 
-        if (level->m_stars == 0 && level->m_starsRequested == 10) m_difficultySprite->updateDifficultyFrame(6, GJDifficultyName::Short);
+        if (level->m_stars.value() == 0 && level->m_starsRequested == 10) m_difficultySprite->updateDifficultyFrame(6, GJDifficultyName::Short);
 
         return true;
     }
@@ -15,19 +15,19 @@ class $modify(NDLevelInfoLayer, LevelInfoLayer) {
     void levelDownloadFinished(GJGameLevel* level) {
         LevelInfoLayer::levelDownloadFinished(level);
 
-        if (level->m_stars == 0 && level->m_starsRequested == 10) m_difficultySprite->updateDifficultyFrame(6, GJDifficultyName::Short);
+        if (level->m_stars.value() == 0 && level->m_starsRequested == 10) m_difficultySprite->updateDifficultyFrame(6, GJDifficultyName::Short);
     }
 
     void levelUpdateFinished(GJGameLevel* level, UpdateResponse response) {
         LevelInfoLayer::levelUpdateFinished(level, response);
 
-        if (level->m_stars == 0 && level->m_starsRequested == 10) m_difficultySprite->updateDifficultyFrame(6, GJDifficultyName::Short);
+        if (level->m_stars.value() == 0 && level->m_starsRequested == 10) m_difficultySprite->updateDifficultyFrame(6, GJDifficultyName::Short);
     }
 
     void likedItem(LikeItemType type, int id, bool liked) {
         LevelInfoLayer::likedItem(type, id, liked);
 
-        if (m_level->m_stars == 0 && m_level->m_starsRequested == 10) m_difficultySprite->updateDifficultyFrame(6, GJDifficultyName::Short);
+        if (m_level->m_stars.value() == 0 && m_level->m_starsRequested == 10) m_difficultySprite->updateDifficultyFrame(6, GJDifficultyName::Short);
     }
 };
 
@@ -37,7 +37,7 @@ class $modify(NDLevelCell, LevelCell) {
         LevelCell::loadFromLevel(level);
 
         if (auto difficultyContainer = m_mainLayer->getChildByID("difficulty-container")) {
-            if (level->m_stars == 0 && level->m_starsRequested == 10)
+            if (level->m_stars.value() == 0 && level->m_starsRequested == 10)
                 static_cast<GJDifficultySprite*>(difficultyContainer->getChildByID("difficulty-sprite"))->updateDifficultyFrame(6, GJDifficultyName::Short);
         }
     }
